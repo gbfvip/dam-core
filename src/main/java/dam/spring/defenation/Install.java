2940ddbb129154063067cf9c993c76c81cf3d353
gaobinfang <gaobinfang@cd.tuan800.com>
2018-01-16 18:35:40 +0800

    simplify Install class

--- src/main/java/dam/spring/defenation/Install.java
+++ src/main/java/dam/spring/defenation/Install.java
@@ -1,30 +1,16 @@
 package dam.spring.defenation;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
-public class Install implements FactoryBean, ApplicationContextAware {
-
-    private ApplicationContext applicationContext;
+public class Install implements FactoryBean {
 
     @Override
     public Object getObject() throws Exception {
-        return applicationContext;
+        return null;
     }
 
     @Override
     public Class<?> getObjectType() {
-        return applicationContext.getClass();
-    }
-
-    @Override
-    public boolean isSingleton() {
-        return true;
-    }
-
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+        return null;
     }
 }
